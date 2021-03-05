215499512fea7e427f331145c6f7b4d0dddd1b33
Christopher Pang <christopherpang24@gmail.com>
2021-03-05 17:18:29 -0500

    Add pre-processing for distillation profile data

--- profile_builder.py
+++ profile_builder.py
@@ -29,9 +29,38 @@ class BlendedProfileBuilder():
 
         profiles_df.to_csv("data/oil-profiles.csv")
 
+    def load_processed_profile(self, code):
+        '''
+        Load distillation profile from file store and process (isolate mass recovery and temperature features)
+        '''
+
+        profile_df = pd.read_csv("data/oil-profiles.csv")
+
+        # isolate relevant columns and rename
+        profile_df = profile_df[['Mass % Recovered', 'Temperature( oC )', 'Code']]
+        profile_df = profile_df[profile_df['Code'] == code].reset_index(drop=True)
+        profile_df.columns = ['Recovery', 'Temperature', 'Code']
+
+        # set initial boiling point (IBP) to 0
+        profile_df['Recovery'][profile_df['Recovery'] == 'IBP'] = 0
+
+        # remove blank recovery point temperature values
+        blank_temps = profile_df[profile_df['Temperature'] == '-'].index
+        profile_df.drop(blank_temps, inplace=True)
+
+        # set data types after cleaning
+        profile_df = profile_df.astype({'Recovery': 'int'})
+        profile_df = profile_df.astype({'Temperature': 'float64'})
+
+        return profile_df
+
     def run(self):
         print("Running Blended Distillation Profile Builder...")
-        self.extract_profiles_from_web()
+        # self.extract_profiles_from_web()
+
+        profile_df = self.load_processed_profile('AHS')
+        print(profile_df)
+
 
 
 if __name__ == "__main__":
