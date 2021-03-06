8a72b905b612f05a8790dfa229cb820c80299bd6
Christopher Pang <christopherpang24@gmail.com>
2021-03-05 19:49:56 -0500

    Review PyLint checks.

--- profile_builder.py
+++ profile_builder.py
@@ -1,9 +1,16 @@
+'''
+Blended Distillation Profile Builder
+'''
+
+import math
 import pandas as pd
 import numpy as np
-import math
 from scipy.interpolate import pchip
 
 class BlendedProfileBuilder():
+    '''
+    Distillation profile builder for blended oil profiles
+    '''
 
     def __init__(self, code1, code2, volume1, volume2):
         self.code1 = code1
@@ -11,7 +18,8 @@ class BlendedProfileBuilder():
         self.volume1 = volume1
         self.volume2 = volume2
 
-    def extract_profiles_from_web(self):
+    @staticmethod
+    def extract_profiles_from_web():
         '''
         Extract distillation profile tables from Crude Monitor website and load into file store
         '''
@@ -35,9 +43,11 @@ class BlendedProfileBuilder():
 
         profiles_df.to_csv("data/oil-profiles.csv")
 
-    def load_processed_profile(self, code):
+    @staticmethod
+    def load_processed_profile(code):
         '''
-        Load distillation profile from file store and process (isolate mass recovery and temperature features)
+        Load distillation profile from file store and process
+        (isolate mass recovery and temperature features)
         '''
 
         profile_df = pd.read_csv("data/oil-profiles.csv")
@@ -60,13 +70,16 @@ class BlendedProfileBuilder():
 
         return profile_df
 
-    def get_discrete_temperature_range(self, df):
+    @staticmethod
+    def get_discrete_temperature_range(profile_df):
         '''
         Compute temperature range based on min/max of profile
         '''
-        return np.arange(math.ceil(df['temperature'].min()), math.floor(df['temperature'].max()), 1)
+        return np.arange(math.ceil(profile_df['temperature'].min()),
+            math.floor(profile_df['temperature'].max()), 1)
 
-    def get_global_temperature_range(self, df1, df2):
+    @staticmethod
+    def get_global_temperature_range(df1, df2):
         '''
         Compute global temperature range based on min/max of profile pair
         '''
@@ -74,41 +87,49 @@ class BlendedProfileBuilder():
         max_val = max(df1['temperature'].max(), df2['temperature'].max())
         return np.arange(math.ceil(min_val), math.floor(max_val), 1)
 
-    def get_recovery_interpolation(self, df):
+    def get_recovery_interpolation(self, profile_df):
         '''
-        Get interpolation over profile range using monotonic cubic splines to find the value of new points
+        Get interpolation over profile range using monotonic cubic splines to find new points
         https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.PchipInterpolator.html
 
-        Using same fit function used by Crude Monitor on distillation profile - interpolation calculator.
+        Using same fit function used by Crude Monitor on distillation
+        profile interpolation calculator.
 
-        Setting temperature as 'x' variable, will get interpolated values for 'Recovery %' over the range.
+        Setting temperature as 'x' variable, will get interpolations
+        for 'Recovery %' over the range.
         '''
-        x = np.array(df['temperature'])
-        y = np.array(df['recovery'])
+        x_values = np.array(profile_df['temperature'])
+        y_values = np.array(profile_df['recovery'])
 
-        range = self.get_discrete_temperature_range(df)
+        temperature_range = self.get_discrete_temperature_range(profile_df)
 
-        interpolation_fit = pchip(x,y)
-        interpolation = pd.DataFrame({'temperature': range, 'recovery': interpolation_fit(range)})
+        interpolation_fit = pchip(x_values,y_values)
+        interpolation = pd.DataFrame({'temperature': temperature_range,
+            'recovery': interpolation_fit(temperature_range)})
         interpolation = interpolation.set_index('temperature')
         return interpolation
 
-    def merge_interpolations_over_range(self, interp1, interp2, range):
+    @staticmethod
+    def merge_interpolations_over_range(interp1, interp2, temperature_range):
         '''
         Merge interpolations from input pair into the specified temperature range
         '''
-        df = pd.DataFrame({'temperature': range})
-        df = df.set_index('temperature')
-        df = df.join(interp1).join(interp2, rsuffix='_2').reset_index()
-        df = df.fillna(method='ffill').fillna(value=0)
-        return df
+        merged_df = pd.DataFrame({'temperature': temperature_range})
+        merged_df = merged_df.set_index('temperature')
+        merged_df = merged_df.join(interp1).join(interp2, rsuffix='_2').reset_index()
+        merged_df = merged_df.fillna(method='ffill').fillna(value=0)
+        return merged_df
 
-    def compute_blended_profile(self, df, share1, share2, df1, df2):
+    @staticmethod
+    def compute_blended_profile(pair_df, share1, share2, df1, df2):
+        '''
+        Compute blended distillation profile using volume share and reversed interpolation
+        '''
 
-        profile_percentages = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99] 
+        profile_percentages = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99]
 
-        df['blended'] = share1 * df['recovery'] + share2 * df['recovery_2']
-        blended_interp = pchip(df['blended'], df['temperature'])
+        pair_df['blended'] = share1 * pair_df['recovery'] + share2 * pair_df['recovery_2']
+        blended_interp = pchip(pair_df['blended'], pair_df['temperature'])
         temperatures = blended_interp(profile_percentages)
 
         blended_df = pd.DataFrame({'recovery': profile_percentages, 'temperature': temperatures})
@@ -118,19 +139,22 @@ class BlendedProfileBuilder():
         blended_df.loc[over_max, 'temperature'] = np.nan
         return blended_df
 
-    def compute_profile(self):
+    def run(self):
+        '''
+        Execute blended distillation profile builder
+        '''
 
         # self.extract_profiles_from_web()
 
         profile1_df = self.load_processed_profile(self.code1)
         profile2_df = self.load_processed_profile(self.code2)
-        
+
         recovery1 = self.get_recovery_interpolation(profile1_df)
         recovery2 = self.get_recovery_interpolation(profile2_df)
 
-        pair_range = self.get_global_temperature_range(profile1_df, profile2_df)
-        paired_recovery = self.merge_interpolations_over_range(recovery1, recovery2, pair_range)
-        blended_df = self.compute_blended_profile(paired_recovery, 
+        global_range = self.get_global_temperature_range(profile1_df, profile2_df)
+        paired_recovery = self.merge_interpolations_over_range(recovery1, recovery2, global_range)
+        blended_df = self.compute_blended_profile(paired_recovery,
             self.volume1, self.volume2, profile1_df, profile2_df)
 
         return blended_df
@@ -139,41 +163,41 @@ if __name__ == "__main__":
 
     print("\nBlended Distillation Profile Builder...")
 
-    profiles_df = pd.read_csv("data/oil-profiles.csv")
-    valid_oil_codes = profiles_df['Code'].unique()
+    valid_profiles_df = pd.read_csv("data/oil-profiles.csv")
+    valid_oil_codes = valid_profiles_df['Code'].unique()
 
     print("\nDistillation profile available for following oil codes:")
     print(valid_oil_codes)
 
     while True:
         print("\nEnter a first vaild oil code:")
-        code1 = input().upper()
-
+        input_code1 = input().upper()
         print("\nEnter a second vaild oil code:")
-        code2 = input().upper()
-
+        input_code2 = input().upper()
         print("\nEnter a volume for the first oil code:")
-        volume1 = input()        
-
+        input_volume1 = input()
         print("\nEnter a volume for the second oil code:")
-        volume2 = input()        
+        input_volume2 = input()
 
-        if code1 not in valid_oil_codes or code2 not in valid_oil_codes:
+        if input_code1 not in valid_oil_codes or input_code2 not in valid_oil_codes:
             print("\nYou entered at least one invalid oil code or code with no data. Try again.")
             continue
 
         try:
-            volume1 = float(volume1)
-            volume2 = float(volume2)
-            total_volume = volume1 + volume2
-            share1 = volume1 / total_volume
-            share2 = volume2 / total_volume            
-        except:
+            input_volume1 = float(input_volume1)
+            input_volume2 = float(input_volume2)
+            total_volume = input_volume1 + input_volume2
+            input_share1 = input_volume1 / total_volume
+            input_share2 = input_volume2 / total_volume
+        except ValueError:
             print("\nYou entered at least one invalid volume. Try again.")
             continue
 
-        builder = BlendedProfileBuilder(code1, code2, share1, share2)
-        blended_profile_df = builder.compute_profile()
+        break
 
-        print(f"\nBlended distillation profile for {code1} ({share1*100:.1f}%) and {code2} ({share2*100:.1f}%):")
-        print(blended_profile_df)
+    builder = BlendedProfileBuilder(input_code1, input_code2, input_share1, input_share2)
+    blended_profile_df = builder.run()
+
+    print(f"\nBlended distillation profile for \
+        {input_code1} ({input_share1*100:.1f}%) and {input_code2} ({input_share2*100:.1f}%):")
+    print(blended_profile_df)
